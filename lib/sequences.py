e9270ec6b7f9aea02b6004666a37fc0ebbbb5f73
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-07-18 13:18:08 -0700

    adds lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+
+    if length >= 1:
+        fibonacci_sequence.append(0)
+    if length >= 2:
+        fibonacci_sequence.append(1)
+
+    while len(fibonacci_sequence) < length:
+        new_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(new_number)
+
+    print(fibonacci_sequence)
